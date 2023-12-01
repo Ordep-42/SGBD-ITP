99e8e4d3433a5a4a3f78b2e2814badd0f6c1dd34
BiancaM <biancamm0107@gmail.com>
2023-11-30 22:13:59 -0300

    EscolhaTipo_Otimizado

--- table.c
+++ table.c
@@ -14,17 +14,22 @@ Tabela criar_tabela() {
         printf("\n%s", separador);
         printf("Digite o nome da %dª coluna: ", i);
         scanf(" %[^\n]", table.colunas[i].nome);
-        printf("\nDigite o tipo da %dª coluna: ", i);
+        printf("\nO tipo da coluna pode ser:"); // (escrito embaixo)
+        printf("\nINT, UNSIGNED_INT, FLOAT, DOUBLE, CHAR ou STRING"); //adicionei as escolhas - bee
 
         char tipo[50];
-        scanf(" %[^\n]", tipo);
-        if (strcmp(tipo, "INT") == 0) table.colunas[i].tipo = INT;
-        else if (strcmp(tipo, "UNSIGNED_INT") == 0) table.colunas[i].tipo = UNSIGNED_INT;
-        else if (strcmp(tipo, "FLOAT") == 0) table.colunas[i].tipo = FLOAT;
-        else if (strcmp(tipo, "DOUBLE") == 0) table.colunas[i].tipo = DOUBLE;
-        else if (strcmp(tipo, "CHAR") == 0) table.colunas[i].tipo = CHAR;
-        else if (strcmp(tipo, "STRING") == 0) table.colunas[i].tipo = STRING;
-    }
+        while(1){ //enquanto o usuario não digitar algo válido ele vai repetir :P - bee
+            printf("\nDigite o tipo desejado da %dª coluna: ", i);
+            scanf(" %[^\n]", tipo);
+            if (strcmp(tipo, "INT") == 0) {table.colunas[i].tipo = INT; break;} //ficou feio ik but foi o único jeito que eu achei de brekar
+            else if (strcmp(tipo, "UNSIGNED_INT") == 0) {table.colunas[i].tipo = UNSIGNED_INT; break;}
+            else if (strcmp(tipo, "FLOAT") == 0) {table.colunas[i].tipo = FLOAT; break;}
+            else if (strcmp(tipo, "DOUBLE") == 0) {table.colunas[i].tipo = DOUBLE; break;}
+            else if (strcmp(tipo, "CHAR") == 0) {table.colunas[i].tipo = CHAR; break;}
+            else if (strcmp(tipo, "STRING") == 0) {table.colunas[i].tipo = STRING; break;}
+            else{printf("Erro!, tipo não encontrado\nTente Novamente:");} //erro adicionado - bee
+        }
+        }
     printf("\n%s", separador);
     printf("Digite o nome da coluna que contem a chave primária: ");
     scanf(" %[^\n]", table.coluna_PK);
