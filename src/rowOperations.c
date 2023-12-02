954f821fa3420bf686a52967bd1bf207bd997628
Ordep-42 <p.galvao@outlook.com.br>
2023-12-02 10:30:26 -0300

    Refatorização completa, todas as funções em CcamelCase, funções de linha e tabelas movidas para seus respectivos headers, mudança do table.h para database.h com as declarações da estrutura de uma tabela.

---
+++ src/rowOperations.c
@@ -0,0 +1,40 @@
+#include "../include/rowOperations.h"
+
+void listarDados(Tabela *table) {
+    /* falta melhorar e mostrar os dados mesmo em vez de só mostrar o nome das colunas
+    printf("%s", separador);
+    for (int i = 0; i < table->num_colunas; i++) {
+        printf("%s\n", table->colunas[i].nome);
+    }
+    printf("%s", separador);
+    */
+}
+
+void adicionarLinha(Tabela *table) {
+    /*
+    for (int i = 0; i < table->num_colunas; i++) {
+        printf("Digite o valor da %dª coluna: ", i);
+        switch (table->colunas[i].tipo) {
+            case UNSIGNED_INT:
+                scanf("%u", &table->linhas[table->num_linhas].celulas[i].valor.unsigned_int);
+                break;
+            case INT:
+                scanf("%d", &table->linhas[table->num_linhas].celulas[i].valor.int_);
+                break;
+            case FLOAT:
+                scanf("%f", &table->linhas[table->num_linhas].celulas[i].valor.float_);
+                break;
+            case DOUBLE:
+                scanf("%lf", &table->linhas[table->num_linhas].celulas[i].valor.double_);
+                break;
+            case CHAR:
+                scanf(" %c", &table->linhas[table->num_linhas].celulas[i].valor.char_);
+                break;
+            case STRING:
+                scanf(" %[^\n]", table->linhas[table->num_linhas].celulas[i].valor.string);
+                break;
+        }
+    }
+    table->num_linhas++;
+    */
+}
