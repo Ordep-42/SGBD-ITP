dfce1d7054017f467fa68608b6915104765b7818
Ordep-42 <p.galvao@outlook.com.br>
2023-12-11 14:47:30 -0300

    Início da implementação da função de adicionar linhas

--- src/rowOperations.c
+++ src/rowOperations.c
@@ -10,31 +10,75 @@ void listarDados(Tabela* table) {
     */
 }
 
-void adicionarLinha(Tabela* table) {
-    /*
-    for (int i = 0; i < table->num_colunas; i++) {
-        printf("Digite o valor da %dª coluna: ", i);
-        switch (table->colunas[i].tipo) {
-            case UNSIGNED_INT:
-                scanf("%u", &table->linhas[table->num_linhas].celulas[i].valor.unsigned_int);
+void adicionarLinha() {
+    char* nomeTabela = (char*) malloc(sizeof(char) * MAX_NAME_SIZE);
+    printf("%s", separador);
+    printf("Digite o nome da tabela que deseja adicionar uma linha: ");
+    scanf(" %[^\n]", nomeTabela);
+    while(!checarTabelaExiste(nomeTabela)) {
+        printf("Tabela não existe. Digite novamente: ");
+        scanf(" %[^\n]", nomeTabela);
+    }
+    char* caminhoTabela = gerarCaminhoDeArquivo(nomeTabela);
+    char* conteudoTabela = lerArquivo(caminhoTabela);
+    char* linhaAtual = strtok(conteudoTabela, "\n");
+    char* linhaNomes;
+    char* linhaTipos;
+    char* linhaPK;
+
+    for (int i = 0; i < 3; i++) {
+        switch (i) {
+            case 0:
+                strncpy(linhaNomes, linhaAtual, strlen(linhaAtual));
                 break;
-            case INT:
-                scanf("%d", &table->linhas[table->num_linhas].celulas[i].valor.int_);
+            case 1:
+                strncpy(linhaTipos, linhaAtual, strlen(linhaAtual));
                 break;
-            case FLOAT:
-                scanf("%f", &table->linhas[table->num_linhas].celulas[i].valor.float_);
-                break;
-            case DOUBLE:
-                scanf("%lf", &table->linhas[table->num_linhas].celulas[i].valor.double_);
-                break;
-            case CHAR:
-                scanf(" %c", &table->linhas[table->num_linhas].celulas[i].valor.char_);
-                break;
-            case STRING:
-                scanf(" %[^\n]", table->linhas[table->num_linhas].celulas[i].valor.string);
+            case 2:
+                strncpy(linhaPK, linhaAtual, strlen(linhaAtual));
                 break;
         }
+        linhaAtual = strtok(NULL, "\n");
     }
-    table->num_linhas++;
-    */
+
+    char** nomes = separarString(linhaNomes);
+    char** tipos = separarString(linhaTipos);
+    char** pks = separarString(linhaPK);
+
+    int numColunas = contarVirgular(linhaNomes);
+    
+    char *novoValor = (char*) malloc(sizeof(char) * MAX_NAME_SIZE);
+    char *novaLinha = (char*) malloc(sizeof(char) * MAX_NAME_SIZE * numColunas);
+
+    for (int i = 0; i < numColunas; i++) {
+        printf("%s", separador);
+        printf("Digite o valor da coluna %s de tipo %s: ", nomes[i], tipos[i]);
+        switch (tipos[i]) {
+            case "UNSIGNED_INT":
+                scanf("%u", novoValor);
+                break;
+            case "INT":
+                scanf("%d", novoValor);
+                break;
+            case "FLOAT":
+                scanf("%f", novoValor);
+                break;
+            case "DOUBLE":
+                scanf("%lf", novoValor);
+                break;
+            case "CHAR":
+                scanf("%c", novoValor);
+                break;
+            case "STRING":
+                scanf(" %[^\n]", novoValor);
+                break;
+            case default:
+                printf("Tipo não reconhecido\n");
+                break;
+        }
+
+        strcat(novaLinha, novoValor);
+        strcat(novaLinha, ",");
+    }
 }
+
