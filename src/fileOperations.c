34dfd0cb68abcf66172bf87bdf5b4aaa1da586b7
Ordep-42 <p.galvao@outlook.com.br>
2023-12-13 09:11:39 -0300

    Documentação das funções de operações com arquivos

--- src/fileOperations.c
+++ src/fileOperations.c
@@ -1,5 +1,13 @@
 #include "../include/fileOperations.h"
 
+/**
+ * @brief Salva o conteúdo em um arquivo.
+ * 
+ * @param nomeArquivo O nome do arquivo a ser salvo.
+ * @param conteudo O conteúdo a ser salvo no arquivo.
+ * @param modo O modo de abertura do arquivo (ex: "w" para escrita, "a" para anexar).
+ * @return 1 se o arquivo foi salvo com sucesso, 0 caso contrário.
+ */
 int salvarEmArquivo(char *nomeArquivo, char *conteudo, char *modo) {
     FILE *arquivo = fopen(nomeArquivo, modo);
     if (arquivo == NULL) {
@@ -11,6 +19,12 @@ int salvarEmArquivo(char *nomeArquivo, char *conteudo, char *modo) {
     return 1;
 }
 
+/**
+ * @brief Função responsável por ler o conteúdo de um arquivo.
+ * 
+ * @param nomeArquivo O nome do arquivo a ser lido.
+ * @return Um ponteiro para o conteúdo do arquivo lido. Retorna NULL em caso de erro.
+ */
 char *lerArquivo(char *nomeArquivo) {
     FILE *arquivo = fopen(nomeArquivo, "r");
     if (arquivo == NULL) {
@@ -42,6 +56,14 @@ char *lerArquivo(char *nomeArquivo) {
     return conteudo;
 }
 
+/**
+ * @brief Função responsável por gerar o caminho de arquivo para um determinado nome.
+ * 
+ * Esta função recebe um nome de um arquivo no banco de dados e gera o caminho de arquivo para ele.
+ * 
+ * @param nome O nome do arquivo.
+ * @return O caminho de arquivo gerado.
+ */
 char *gerarCaminhoDeArquivo(char *nome) {
     char *caminho = malloc(sizeof(char) * 64);
     strcpy(caminho, "./data/");
@@ -50,6 +72,16 @@ char *gerarCaminhoDeArquivo(char *nome) {
     return caminho;
 }
 
+
+/**
+ * @brief Separa uma string em um array de substrings com base em um delimitador.
+ * 
+ * Esta função recebe uma string e a divide em várias substrings usando um delimitador.
+ * As substrings são armazenadas em um array de strings alocado dinamicamente.
+ * 
+ * @param string A string a ser separada.
+ * @return char** Um array de strings contendo as substrings separadas.
+ */
 char **separarString(char *string) {
     char *stringCopy = strdup(string); // Create a copy of the original string
     char **resultado = NULL;
@@ -73,6 +105,12 @@ char **separarString(char *string) {
     return resultado;
 }
 
+/**
+ * @brief Apaga todas as linhas de um arquivo que correspondem a um determinado conteúdo.
+ *
+ * @param nomeArquivo O nome do arquivo a ser modificado.
+ * @param conteudoLinha O conteúdo da linha a ser removida.
+ */
 void apagarLinhaPorConteudo(char *nomeArquivo, char *conteudoLinha) {
     char *conteudo = lerArquivo(nomeArquivo);
     char *linhaAtual = strtok(conteudo, "\n");
@@ -92,6 +130,12 @@ void apagarLinhaPorConteudo(char *nomeArquivo, char *conteudoLinha) {
     free(novoConteudo);
 }
 
+/**
+ * @brief Função que conta o número de linhas em uma string.
+ * 
+ * @param conteudo A string contendo o conteúdo a ser contado.
+ * @return O número de linhas na string.
+ */
 int contarLinhas(char *conteudo) {
     int linhas = 0;
     for (int i = 0; i < strlen(conteudo); i++) {
@@ -102,6 +146,12 @@ int contarLinhas(char *conteudo) {
     return linhas;
 }
 
+/**
+ * @brief Conta o número de vírgulas em uma string.
+ * 
+ * @param conteudo A string na qual as vírgulas serão contadas.
+ * @return O número de vírgulas encontradas na string.
+ */
 int contarVirgulas(char *conteudo) {
     int virgulas = 0;
     for (int i = 0; i < strlen(conteudo); i++) {
