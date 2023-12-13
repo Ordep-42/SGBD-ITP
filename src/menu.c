73f12481a4d1685688e9241d59726b3ed528f84d
BiancaM <93006697+Bia1000b@users.noreply.github.com>
2023-12-13 00:39:45 -0300

    Printar tabela 100% working (#33)
    Co-authored-by: Pedro Galvão do Amaral Neto <90785456+Ordep-42@users.noreply.github.com>
    Co-authored-by: Ordep-42 <p.galvao@outlook.com.br>

--- src/menu.c
+++ src/menu.c
@@ -2,44 +2,53 @@
 #include "../include/menu.h"
 
 void menuInicial(){
-    int acao;
-    printf("------------Bem Vindo(a)------------\n");
-    printf("----------------MENU----------------\n");
-    printf("O que gostaria de fazer?\n");
-    printf("1 - Criar uma nova tabela\n");
-    printf("2 - Listar todas as tabelas\n");
-    printf("3 - Listar dados de uma tabela\n");
-    printf("4 - Pesquisar valor em uma tabela\n");
-    printf("5 - Apagar uma tupla de uma tabela\n");
-    printf("6 - Deletar uma tabela\n");
-    printf("Digite o numero da acao desejada:\n");
-    while(1){
-        scanf("%d",&acao);
-        if (acao<1 || acao>6){
-            printf("Erro! O numero digitado nao corresponde a nenhuma funcao\nTente Novamente:");
+     int acao;
+    while (1) {
+        printf("%s", separador);
+        printf("------------Bem Vindo(a)------------\n");
+        printf("----------------MENU----------------\n");
+        printf("O que gostaria de fazer?\n");
+        printf("1 - Criar uma nova tabela\n");
+        printf("2 - Listar todas as tabelas\n");
+        printf("3 - Listar dados de uma tabela\n");
+        printf("4 - Adicionar uma tupla em uma tabela\n");
+        printf("5 - Pesquisar valor em uma tabela\n");
+        printf("6 - Apagar uma tupla de uma tabela\n");
+        printf("7 - Deletar uma tabela\n");
+        printf("8 - Sair\n");
+        printf("Digite o numero da acao desejada:\n");
+        while (1) {
+            scanf("%d", &acao);
+            if (acao < 1 || acao > 8) {
+                printf("Erro! O numero digitado nao corresponde a nenhuma funcao\nTente Novamente:");
+            } else {
+                break;
+            }
         }
-        else{
-            break;
-        }
-    }
-    switch (acao) {
+        switch (acao) {
         case 1:
-            Tabela teste = criarTabela();
+            criarTabela();
             break;
         case 2:
-            //função listar tabelas
+            listarTabelas();
             break;
         case 3:
-            //função listar dados
+            printarTabela();
             break;
         case 4:
-            //função pesquisar valor
+            //adicionarLinha();
             break;
         case 5:
-            //função apagar tupla
+            // função pesquisar valor
             break;
         case 6:
-            //função deletar lista
+            // função deletar tupla
             break;
+        case 7:
+            apagarTabela();
+            break;
+        case 8:
+            return 0;
+        }
     }
 }
